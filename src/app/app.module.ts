a32226658d2981c5eaf434c2842924144dd935d7
Álvaro Bunster <al.bunstr@gmail.com>
2018-09-19 17:22:53 -0300

    Aplicación completa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProtegidaComponent } from './components/protegida/protegida.component';
+import { PreciosComponent } from './components/precios/precios.component';
+
+// Rutas
+import {APP_ROUTING} from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ProtegidaComponent,
+    PreciosComponent
   ],
   imports: [
-    BrowserModule
+      BrowserModule,
+      APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
